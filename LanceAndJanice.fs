6a3b74fad5d34811ecfe324baa938a5afd5eaf00
Heinrich G Henning <heinrichgh@gmail.com>
2020-12-05 17:51:02 +0200

    Initial commit

--- LanceAndJanice.fs
+++ LanceAndJanice.fs
@@ -1,2 +1,36 @@
 ﻿module FooBarQ.LanceAndJanice
 
+open System
+
+let alphabet = [|'a'..'z'|]
+
+let testCases = [|
+    "wrw blf hvv ozhg mrtsg'h vkrhlwv?";
+    "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
+|]
+
+let dictionary = Array.zip alphabet (Array.rev alphabet)
+
+let dictionaryLookup char =
+    match char with
+    | a when a >= 'a' && a <= 'z' ->
+                            (
+                                dictionary
+                                |> Array.filter (fun (a, b) -> a = char)
+                                |> Array.map snd
+                            ).[0]
+    | _ -> char
+    
+
+let decrypt (message:string) =
+    message
+    |> Seq.map dictionaryLookup
+
+let answers =
+    testCases
+    |> Array.map
+           (
+            decrypt
+            >> Seq.toArray
+            >> String
+           )
--- Program.fs
+++ Program.fs
@@ -1,13 +1,6 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+﻿open FooBarQ.LanceAndJanice
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn "%A" answers
     0 // return an integer exit code
